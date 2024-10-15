40e678856f8cdd788b078388d0cf0079da79fc2e
vladi-klmv <126790409+vladi-klmv@users.noreply.github.com>
2024-10-15 09:24:58 +0300

    logout

--- screens/ProfileScreen.tsx
+++ screens/ProfileScreen.tsx
@@ -1,11 +1,12 @@
 import storage from "@react-native-firebase/storage";
-
+import auth from "@react-native-firebase/auth";
 import {
   TouchableOpacity,
   StyleSheet,
   Alert,
   ActivityIndicator,
   Text,
+  View,
 } from "react-native";
 import { useForm, Controller } from "react-hook-form";
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -13,10 +14,11 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import TextInput from "../components/TextInput";
 import UploadImage from "../components/UploadImage";
 import { useUserContext } from "../context/userContext";
-import { useEffect } from "react";
 import { updateProfile, updateProfileImage } from "../services/profile.service";
+import { useNavigation } from "@react-navigation/native";
 
 export default function App() {
+  const navigation = useNavigation();
   const { user, profile, setProfile, profileImage, setProfileImage } =
     useUserContext();
   const {
@@ -53,55 +55,69 @@ export default function App() {
     }
   };
 
+  const onLogout = async () => {
+    await auth().signOut();
+  };
+
   return (
     <SafeAreaView style={styles.container}>
-      <UploadImage
-        image={image || profileImage}
-        name="image"
-        onChange={onChangeImage}
-      />
-      <Controller
-        control={control}
-        render={({ field: { onChange, onBlur, value } }) => (
-          <TextInput
-            placeholder="Username"
-            onBlur={onBlur}
-            onChange={onChange}
-            value={value}
-          />
-        )}
-        name="name"
-      />
-      <Controller
-        control={control}
-        render={({
-          field: { onChange, onBlur, value },
-          fieldState: { error },
-        }) => (
-          <TextInput
-            placeholder="Phone"
-            onBlur={onBlur}
-            onChange={onChange}
-            value={value}
-            keyboardType="phone-pad"
-            error={error?.message}
-          />
-        )}
-        rules={{
-          pattern: {
-            value: /^\+[1-9]\d{1,14}$/,
-            message: "Invalid phone number format. Must be 10-14 digits.",
-          },
-        }}
-        name="phone"
-      />
+      <View>
+        <UploadImage
+          image={image || profileImage}
+          name="image"
+          onChange={onChangeImage}
+        />
+        <Controller
+          control={control}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <TextInput
+              placeholder="Username"
+              onBlur={onBlur}
+              onChange={onChange}
+              value={value}
+            />
+          )}
+          name="name"
+        />
+        <Controller
+          control={control}
+          render={({
+            field: { onChange, onBlur, value },
+            fieldState: { error },
+          }) => (
+            <TextInput
+              placeholder="Phone"
+              onBlur={onBlur}
+              onChange={onChange}
+              value={value}
+              keyboardType="phone-pad"
+              error={error?.message}
+            />
+          )}
+          rules={{
+            pattern: {
+              value: /^\+[1-9]\d{1,14}$/,
+              message: "Invalid phone number format. Must be 10-14 digits.",
+            },
+          }}
+          name="phone"
+        />
 
-      <TouchableOpacity
-        onPress={handleSubmit(onSubmit)}
-        style={styles.saveButton}
-      >
-        <Text style={styles.saveButtonnText}>Save</Text>
-      </TouchableOpacity>
+        <TouchableOpacity
+          onPress={handleSubmit(onSubmit)}
+          style={styles.saveButton}
+        >
+          <Text style={styles.saveButtonnText}>Save</Text>
+        </TouchableOpacity>
+      </View>
+      <View>
+        <TouchableOpacity
+          onPress={handleSubmit(onLogout)}
+          style={styles.logoutButton}
+        >
+          <Text style={styles.saveButtonnText}>Logout</Text>
+        </TouchableOpacity>
+      </View>
       <ActivityIndicator
         style={styles.activityIndicator}
         size="large"
@@ -116,6 +132,7 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 16,
     backgroundColor: "#fff",
+    justifyContent: "space-between",
   },
   input: {
     height: 40,
@@ -129,6 +146,16 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     backgroundColor: "#2196F3",
+    padding: 14,
+    borderRadius: 50,
+    marginVertical: 20,
+    width: 160,
+    alignSelf: "center",
+  },
+  logoutButton: {
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#f41603",
     padding: 14,
     borderRadius: 50,
     marginVertical: 20,
